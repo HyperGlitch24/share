3b4f43c90ed8a7ab0a1fdef4a8642f7fd45ca7fa
Vamsi <vamsiteja2412@gmail.com>
2023-08-19 09:34:20 +0530

    responsive

--- ClientData.js
+++ ClientData.js
@@ -2,7 +2,7 @@ import './ClientData.scss'
 
 const ClientData =()=>{
     return (
-        <div className='container'>
+        <div className='container1'>
             <div className='client'>
               <img className='img' src="https://via.placeholder.com/70x70"/>
               <div className='name'>Ronald</div>
@@ -28,6 +28,75 @@ const ClientData =()=>{
                     </div>
                     
                 </div>
+                <div className='row'>
+                    <div className='element'>
+                       <h1>
+                        Gender
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    <div className='element'>
+                       <h1>
+                        Height
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    
+                </div>
+            
+                <div className='row'>
+                    <div className='element'>
+                       <h1>
+                        Age
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    <div className='element'>
+                       <h1>
+                        About
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    
+                </div>
+                <div className='row'>
+                    <div className='element'>
+                       <h1>
+                        Weight
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    <div className='element'>
+                       <h1>
+                        Duration
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    
+                </div>
+                <div className='row'>
+                    <div className='element'>
+                       <h1>
+                        Plan
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    <div className='element'>
+                       <h1>
+                        Location
+                       </h1>
+                       <input/>
+                    
+                    </div>
+                    
+                </div>
 
             </div>
 
@@ -36,6 +105,15 @@ const ClientData =()=>{
 
 
             <div className='buttons'>
+
+                <div className='btn1'>
+                    <button>Go Back</button>
+
+                </div>
+                <div className='btn2'>
+                    <button>Remove Client</button>
+
+                </div>
 
             </div>
 
